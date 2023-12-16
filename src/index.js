673daf6f93b439930b97c06e6a22655504d2bf5b
Vitor Linaris <vitor.linaris@gmail.com>
2023-12-15 23:24:09 -0300

    feat: add hero

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import imgLogo from "./assets/img/logo.png";
 import imgLogin from "./assets/img/login.svg";
 import imgShop from "./assets/img/shop.svg";
 import imgUnion from "./assets/img/union.svg";
+import imgApp from "./assets/img/icon-app.svg";
 
 const imgLogoMobile = document.getElementById("imgLogoMobile");
 imgLogoMobile.src = imgLogo;
@@ -20,3 +21,6 @@ imgShopDesktop.src = imgShop;
 
 const imgUnionDesktop = document.getElementById("imgUnion");
 imgUnionDesktop.src = imgUnion;
+
+const imgAppDesktop = document.getElementById("imgAppDesktop");
+imgAppDesktop.src = imgApp;
